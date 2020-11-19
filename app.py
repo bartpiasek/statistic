ab7cb53a12d6c41fb71b26b77867840cc1e7c5c9
Bart Piasek <bartpiasek@icloud.com>
2020-11-19 11:36:21 +0100

    CONFIG: input - punktowa

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
-
+import inquirer
 
 # CHOOSE 'CECHA CIĄGŁA, CECHA NASTĘPNA, PUNKTOWA
+options = [
+    inquirer.List('cecha', 
+    message = 'Jaka cecha Xi?',
+    choices = ['Punktowa', 'Ciągła', 'Następna'],
+    ),
+]
+answers = inquirer.prompt(options)
+print(answers['cecha'])
+# IF PUNKTOWA - INPUT X  - list
+if answers['cecha'] == 'Punktowa':
+    numberList = []
 
-# IF PUNKTOWA - INPUT X NUM
+    n = int(input("Enter the size of the list "))
+print("\n")
+numberList = list(int(num) for num in input("Enter the list numbers separated by space ").strip().split())[:n]
+print("User List: ", numberList)
 
-#IF CIĄGŁA, NASTĘPNA - INPUT (X - Y), (X - Y)
+#IF CIĄGŁA, NASTĘPNA - INPUT (X - Y), (X - Y) - dict
 
 # INPUT NUM OF - Ni, CZYLI ILOŚĆ
 
 # COUNT - ŚREDNIA ARYTMETYCZNA
-# 
+ 
 # COUNT - ŚRODEK PRZEDZIAŁÓW - ŚREDNIA ARYTMETYCZNA
-# 
+
 # COUNT -  (ŚRODEK PRZEDZIAŁÓW - ŚREDNIA ARYTMETYCZNA)^2 * Ni
 
+# COUNT -  (ŚRODEK PRZEDZIAŁÓW - ŚREDNIA ARYTMETYCZNA)^3 * Ni
 
+# COUNT -  (ŚRODEK PRZEDZIAŁÓW - ŚREDNIA ARYTMETYCZNA)^4 * Ni
+
+# COUNT - ODCHYLENIE STANDARDOWE
+
+# COUNT - WSPÓŁCZYNNIK ZMIENNOŚCI Vx
+
+# COUNT - WSPÓŁCZYNNIK ASYMETRII
+
+# COUNT - WSPÓŁCZYNNIK KONCENTRACJI
 
 # IF CIĄGŁA, NASTĘPNA - COUNT 'ŚRODEK PRZEDZIAŁÓW'
