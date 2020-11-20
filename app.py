6e706012bf3061827afc93e3615ef1c82b462182
Bart Piasek <bartpiasek@icloud.com>
2020-11-20 23:19:53 +0100

    DONE: class avg

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import inquirer
+import statistics
 
 # CHOOSE 'CECHA CIĄGŁA, CECHA NASTĘPNA, PUNKTOWA
 options = [
@@ -10,13 +11,44 @@ options = [
 answers = inquirer.prompt(options)
 print(answers['cecha'])
 # IF PUNKTOWA - INPUT X  - list
-if answers['cecha'] == 'Punktowa':
-    numList = []
+def statMenu():
+    """
+    Menu with stat options and inputs
+    """
+    if answers['cecha'] == 'Punktowa':
+        numList = []
 
-n = int(input("Enter the size of the list "))
-print("\n")
-numList = list(int(num) for num in input("Enter the list numbers separated by space ").strip().split())[:n]
-print("User List: ", numList)
+        n = int(input("Enter the size of the list: "))
+        print("\n")
+        # COMPREHENSION LIST
+        numList = list(int(num) for num in input("Enter the list numbers separated by space ").strip().split())[:n]
+        print(numList)
+        return numList
+    else:
+        final_list = [] 
+        line = int(input("Enter the list of tuples: "))
+        print("\n")
+        # WHILE LOOP
+        while(line != ''): 
+            final_list.append(tuple(line.split())) 
+            line = input() 
+
+        print(final_list)
+        return final_list
+
+class Avg():
+    """
+    Count avg of pkt or cgl
+    """
+    def avgPkt(numList):
+        AvgDataPkt = statistics.mean(numList)
+        return AvgDataPkt
+    
+    def avgPkt(final_list):
+        AvgDataCgl = statistics.mean(final_list)
+        return AvgDataCgl
+    
+
 
 
 #IF CIĄGŁA, NASTĘPNA - INPUT (X - Y), (X - Y) - dict
